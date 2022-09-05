e737f30614f54e1e0d55ae20ff2bc5eaf51613c4
Vinícius Silva <xdenik13@gmail.com>
2022-09-04 21:06:51 -0300

    terminando program.

--- programacao_completa/Course/application/Program.java
+++ programacao_completa/Course/application/Program.java
@@ -26,6 +26,19 @@ public class Program {
 		
 		System.out.println();
 		System.out.println("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		
+		System.out.println();
+		System.out.println("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
 		
 
 		sc.close();
