964a7da65e32f6c5989fed443ed347d2d89c3e27
Vinícius Silva <xdenik13@gmail.com>
2022-09-08 11:58:03 -0300

    Introdução construtor

--- programacao_completa/Course/application/ex_Program.java
+++ programacao_completa/Course/application/ex_Program.java
@@ -3,16 +3,16 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.Product;
+import entities.ex_Product;
 
-public class Program {
+public class ex_Program {
 
 	public static void main(String[] args) {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		Product product = new Product();
+		ex_Product product = new ex_Product();
 		System.out.println("Enter product data: ");
 		System.out.print("Name: ");
 		product.name = sc.nextLine();
--- programacao_completa/Course/entities/ex_Product.java
+++ programacao_completa/Course/entities/ex_Product.java
@@ -1,6 +1,6 @@
 package entities;
 
-public class Product {
+public class ex_Product {
 	
 	public String name;
 	public double price;
