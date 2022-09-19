25ff4f800db3c8886181d0ddf60fe40cd93eb75c
Akom4n <xdenik13@gmail.com>
2022-09-19 14:01:08 -0300

    att seção 10

---
+++ programacao_completa/Vet_Array_List/Comportamentos/BoxingUnboxing_WrapperClass.java
@@ -0,0 +1,33 @@
+package Comportamentos;
+
+public class BoxingUnboxing_WrapperClass {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Boxing: 
+           	 É o processo de conversão de um objeto tipo valor para um objeto tipo referência compatível
+           	 
+           	 int x = 20;
+           	 object obj = x;
+           	 
+           	 
+		 * Unboxing:
+		  	 É o processo de conversão de um objeto tipo referência para um objeto tipo valor compatível
+		  	 
+		  	 int y = (int) obj;
+		  	 
+  	 
+		 * Wrapper classes:
+		  	 São classes equivalentes aos tipos primitivos
+		  	 Boxing e unboxing é natural na linguagem
+		  	 Uso comum: campos de entidades em sistemas de informação(IMPORTANTE!)
+		  	 	Pois tipos referência (classes) aceitam valor null e usufruem dos recursos OO
+		  	 	
+		  	Number - Byte(bite), Short(short), Integer(int), Long(long), Float(float), Double(double) - Object
+		  	
+		  	Object - Boolean(boolean), Character(char). 
+		 */
+		
+	}
+}
---
+++ programacao_completa/Vet_Array_List/Comportamentos/lacoForEach.java
@@ -0,0 +1,22 @@
+package Comportamentos;
+
+public class lacoForEach {
+	
+	public static void main(String[] args) {
+		
+		/*
+		 * Laço "for each"
+		   
+		    Sintaxe opcional e simplificada para percorrer coleções
+		    
+		    Sintaxe:
+		    
+		    for (Tipo apelido : coleção) {
+		    	<comando 1>
+		    	<comando 2>
+		    }
+		 * 
+		 */
+	}
+
+}
