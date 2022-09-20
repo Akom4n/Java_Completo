7d6c224b04159e0744cc9038e62149f7656191fc
Akom4n <xdenik13@gmail.com>
2022-09-20 15:27:17 -0300

    revendo um pouco de matrizes

---
+++ programacao_completa/Vet_Array_List/Comportamentos/Matrizes.java
@@ -0,0 +1,27 @@
+package Comportamentos;
+
+public class Matrizes {
+	
+	public static void main(String[] args) {
+		
+		/*
+		 * MATRIZES:
+		  
+		  Em programação, "matriz" é o nome dado a arranjos bidimensionais
+		    atenção: "veotr de vetores"
+		    
+		  Arranjo (array) é uma estrutura de dados:
+		  	Homogênea (dados do mesmo tipo)
+		  	Ordenada (elementos acessados por meio de posições)
+		  	Alocada de uma vez só, em um bloco contíguo de memória
+		  	
+		  
+		  Vantagens:
+		  	Acesso imediato aos elementos pela sua posição
+		  	
+		  Desvantagens:
+		  	Tamanho fixo
+		  	Dificuldade para se realizar inserções e deleções
+		 */
+	}
+}
